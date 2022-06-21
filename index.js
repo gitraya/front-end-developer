1443f8f6e007afb0b4607b14b208d1ff270f1ab9
Raya <mail.raya15@gmail.com>
2022-06-21 22:15:47 +0700

    add server and index page

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+routes.forEach((route) => {
+  return app.use(
+    `/${route}`,
+    express.static(path.join(__dirname, `${route}/build`))
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
