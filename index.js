58a8fe4c93b3e71325043a7c72e75f0f73ed036f
Raya <mail.raya15@gmail.com>
2022-06-22 06:31:49 +0700

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
